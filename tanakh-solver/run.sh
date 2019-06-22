6e412527d3fc8927f6df390f53d38c1d9f9ee238
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2019-06-22 23:57:03 +0900

    MOP

--- tanakh-solver/run.sh
+++ tanakh-solver/run.sh
@@ -5,10 +5,10 @@ cargo build --release
 rm -rf tmp
 mkdir tmp
 
-for i in $(seq 1 300); do
+for i in $(seq 220 300); do
     input=$(printf "data/prob-%03d.desc" $i)
     printf "Solving $input ...\n"
-    target/release/tanakh-solver solve $input
+    target/release/tanakh-solver solve --increase-mop $input
 done
 
 # cd tmp
