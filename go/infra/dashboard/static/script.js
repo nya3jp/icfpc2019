ac32cfec5514457c63a9ee23c5d2038d23d1df34
chiro <i@chir.jp>
2019-06-23 16:49:22 +0900

    Add ranking graph page for each round.

---
+++ go/infra/dashboard/static/script.js
@@ -0,0 +1,54 @@
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(draw);
+
+data_url = 'https://storage.googleapis.com/sound-type-system/rankings/ranking_data.json'
+
+function draw() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var raw_data = JSON.parse(this.responseText);
+      drawRanking(raw_data);
+    }
+  };
+  xmlhttp.open("GET", data_url, true);
+  xmlhttp.send();
+}
+
+function drawRanking(raw_data) {
+  var data = new google.visualization.DataTable();
+  rounds = 0;
+  data.addColumn('number', 'Round');
+  raw_data = raw_data.slice(0, 9);
+  raw_data.forEach(function(elem) {
+    for (team in elem) {
+      data.addColumn('number', team);
+      rounds = elem[team].length
+    }
+  });
+
+   rows = [];
+   for (var i = 0; i < rounds; ++i) {
+      rows.push([i]);
+      raw_data.forEach(function(elem) {
+        for (team in elem) {
+          rows[i].push(elem[team][i]);
+        }
+      });
+   }
+   console.log(rows);
+   data.addRows(rows);
+
+   var options = {
+     hAxis: {
+       title: 'Round'
+     },
+     vAxis: {
+       title: 'Rank'
+     }
+   };
+
+   var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+   chart.draw(data, options);
+}
