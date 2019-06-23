9346094a19675c48c4879af97eb210be205ef606
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2019-06-23 17:00:23 +0900

    Uprev lambda.js

--- reference_checker/check.js
+++ reference_checker/check.js
@@ -4,12 +4,13 @@ const process = require('process');
 const lib = require('./lambda.js');
 
 
-if (process.argv.length !== 4) {
-  console.error("Usage: nodejs validator.js [task] [solution]");
+if (process.argv.length < 4) {
+  console.error("Usage: nodejs validator.js [task] [solution] [purchase]");
   process.exit(1);
 }
 
 const task = fs.readFileSync(process.argv[2]);
 const solution = fs.readFileSync(process.argv[3]);
+const purchase = process.argv[4];
 
-console.log(lib.check(task, solution));
+console.log(lib.check(task, solution, purchase));
--- reference_checker/index.js
+++ reference_checker/index.js
@@ -1,12 +1,12 @@
 const lib = require('./lambda.js');
 
 exports.check = async function(req, res) {
-  const { task, solution } = req.body;
+  const { task, solution, purchase } = req.body;
   if (!task || !solution) {
     res.status(400).send('Required parameters not set');
     return
   }
 
-  const output = lib.check(task, solution);
+  const output = lib.check(task, solution, purchase);
   res.status(200).send(output);
 };
