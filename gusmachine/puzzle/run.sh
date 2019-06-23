d48e6033e15f0b5d68fa16105e94077548a7c582
Yu Sugawara <gusmachine+git@gmail.com>
2019-06-23 15:22:44 +0000

    Make solve-puzzle runnable.

--- gusmachine/puzzle/run.sh
+++ gusmachine/puzzle/run.sh
@@ -2,5 +2,7 @@
 # One line explanation of run.sh.
 entry="$1"
 input="../../lambda-client/blocks/${entry}/puzzle.cond"
-result="results/${entry}/puzzle."
+result="results/${entry}/puzzle.desc"
+mkdir -p results/${entry}
 ./solve-puzzle < "$input" > "$result"
+nodejs ../../puzzle_checker/check.js "$input" "$result"
